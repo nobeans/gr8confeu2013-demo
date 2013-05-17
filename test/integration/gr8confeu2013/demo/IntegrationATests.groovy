4a8213ae33a4a0fd20c0ebd35ac8cf1f5877588a
Yasuharu NAKANO <ynak@jggug.org>
2013-05-17 15:17:21 +0900

    just generated tests

---
+++ test/integration/gr8confeu2013/demo/IntegrationATests.groovy
@@ -0,0 +1,22 @@
+package gr8confeu2013.demo
+
+import static org.junit.Assert.*
+import org.junit.*
+
+class IntegrationATests {
+
+    @Before
+    void setUp() {
+        // Setup logic here
+    }
+
+    @After
+    void tearDown() {
+        // Tear down logic here
+    }
+
+    @Test
+    void testSomething() {
+        fail "Implement me"
+    }
+}
