d34b8aea3b62a31c8a32cd382658a293589aa7ff
Yasuharu NAKANO <ynak@jggug.org>
2013-05-20 18:18:27 +0900

    removed @GrailsUnitTestMixin from unit tests

--- test/unit/gr8confeu2013/demo/UnitATests.groovy
+++ test/unit/gr8confeu2013/demo/UnitATests.groovy
@@ -1,10 +1,7 @@
 package gr8confeu2013.demo
 
-import grails.test.mixin.TestMixin
-import grails.test.mixin.support.GrailsUnitTestMixin
 import org.junit.Test
 
-@TestMixin(GrailsUnitTestMixin)
 class UnitATests {
 
     @Test
